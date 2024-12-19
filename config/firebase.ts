cb1c47f04eb4edb70faf866190d7df7d9c98f050
Jonah0705 <jonahpupella@gmail.com>
2024-12-19 20:46:44 +0700

    [FIX]

--- config/firebase.ts
+++ config/firebase.ts
@@ -4,14 +4,9 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsFiE3A9NnTXAIJSqdRj3m_XU8PgHvtGg",
-  authDomain: "travel-10474.firebaseapp.com",
-  projectId: "travel-10474",
-  storageBucket: "travel-10474.firebasestorage.app",
-  messagingSenderId: "155347889323",
-  appId: "1:155347889323:web:0651d6e0ed5366ba091bee",
-  measurementId: "G-MBE7XP6FPD"
+
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
